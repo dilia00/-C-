5e76ed86cd22a9aeda702e0ff3c63149ec532734
dilia00 <dina_xenia@mail.ru>
2022-09-15 00:55:49 +0300

    add Example005_If_Else project

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите свое имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "дина")
+{
+    Console.WriteLine("О! Да это же Дина!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.Write("!");
+}
