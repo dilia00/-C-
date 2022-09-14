6bf056421ead26e3da22fe9e84e31f20878a56e0
dilia00 <dina_xenia@mail.ru>
2022-09-15 01:09:27 +0300

    add Example006_Bese project

---
+++ Example006_Bese/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 2;
+int b = 8;
+int c = 5;
+int d = 4;
+int e = 9;
+int max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+Console.Write("max = ");
+Console.Write(max);
