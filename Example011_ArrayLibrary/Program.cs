f4bc03695296a28b9b9b7e0a6f025d26725b263f
dilia00 <dina_xenia@mail.ru>
2022-09-18 22:20:27 +0300

    add Example011_ArrayLibrary + IndexOf

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -2,7 +2,7 @@
 {
     int length = collection.Length;
     int index = 0;
-    while (index<length)
+    while (index < length)
     {
         collection[index] = new Random().Next(1, 20);
         index++;
@@ -13,13 +13,33 @@ void PrintArray(int[] col)
 {
     int count = col.Length;
     int position = 0;
-    while (position<count)
+    while (position < count)
     {
         Console.WriteLine(col[position]);
         position++;
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int [] array = new int [10];
 FullArray(array);
 PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 5);
+Console.WriteLine(pos);
