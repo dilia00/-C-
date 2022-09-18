24c914d8a7fdb20669aebaa6c10fc81d3382182e
dilia00 <dina_xenia@mail.ru>
2022-09-18 21:10:57 +0300

    add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {15,22,3,42,54,116,7,18,29};
+int n = array.Length;
+int find = 3;
+int index = 0;
+while (index < n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
